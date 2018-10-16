eef123b31a2b7388e478cb3fe1f728400a9023d7
Yuki Sumida <y.sumida.0201@gmail.com>
2018-10-16 22:44:12 +0900

    プロジェクト作成

---
+++ NavigationTitleColorSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationTitleColorSample
+//
+//  Created by Yuki Sumida on 2018/10/16.
+//  Copyright © 2018年 Yuki Sumida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
