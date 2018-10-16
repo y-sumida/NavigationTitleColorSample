3b99e0ac54f54c7f7dc4ad4da3d635edb85e6d54
Yuki Sumida <y.sumida.0201@gmail.com>
2018-10-16 23:06:19 +0900

    戻ってきたときに元の色に戻す

--- NavigationTitleColorSample/ViewController.swift
+++ NavigationTitleColorSample/ViewController.swift
@@ -17,6 +17,18 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.red]
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // こっちは効いてないどころか、指定するとviewDidAppearも効かなくなる
+        //navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.red]
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // 効くけど、viewDidAppearなので、一瞬前の色が見える
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.red]
+    }
+
     @IBAction func tapButton(_ sender: Any) {
         let vc: ViewController2  = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController2") as! ViewController2
         self.navigationController?.pushViewController(vc, animated: true)
