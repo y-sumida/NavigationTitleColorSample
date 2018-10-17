739610c141122416cbadaa5c472be29bf79bdd2e
Yuki Sumida <y.sumida.0201@gmail.com>
2018-10-17 21:35:04 +0900

    遷移時に遷移元画面のタイトルカラーを保存してみる

--- NavigationTitleColorSample/ViewController2.swift
+++ NavigationTitleColorSample/ViewController2.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class ViewController2: UIViewController {
+    private var originalAttr: [NSAttributedString.Key : Any]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "ViewController2"
+        // redじゃなかった
+        originalAttr = navigationController?.navigationBar.titleTextAttributes
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.blue]
     }
 
     override func willMove(toParent parent: UIViewController?) {
         super.willMove(toParent: parent)
         // いい感じ
-        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.red]
+        //navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.blue]
+        navigationController?.navigationBar.titleTextAttributes = originalAttr
     }
 }
